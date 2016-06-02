dd0a714638084a061dd84e81e91bc3e06758531b
David Bessent <davidmbessent@gmail.com>
2016-06-01 23:15:32 -0400

    Initial Commit

---
+++ Cadou/Cadou/HelloWorld.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Cadou
+{
+    public partial class HelloWorld : ContentPage
+    {
+        public HelloWorld()
+        {
+            InitializeComponent();
+        }
+    }
+}
