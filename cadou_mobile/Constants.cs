f94ad6a7121ff5b63484ae55b40edb2e2f5d115b
David Bessent <davidmbessent@gmail.com>
2016-11-05 19:59:54 -0400

    A brand new start
    Took example project from Azure Portal

---
+++ cadou_mobile/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace cadou_mobile
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://cadou-mobile.azurewebsites.net";
+	}
+}
+
